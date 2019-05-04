6c7b4770ac1286ca64e2463ea0ae587faf6ecb76
Robin <robin.e.melanson@gmail.com>
2019-05-04 12:18:23 -0400

    WIP

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -2,12 +2,34 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World";
-    }
+
+	@Autowired
+	 private Environment env;
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@RequestMapping("/port")
+	public String helloport() {
+		String port = getPropertyValue("server.port");
+		return "Hello World from port " + port;
+	}
+
+	public String getPropertyValue(@RequestParam("key") String key) {
+		String returnValue = "No value";
+		String keyValue = env.getProperty(key);
+
+		if (keyValue != null && !keyValue.isEmpty()) {
+			returnValue = keyValue;
+		}
+		return returnValue;
+	}
 }
